bd0a3ccec915dbdc53b3a304922bf915760ab493
Voragun Supawong <voragun2543@gmail.com>
2021-06-08 22:48:17 +0700

    [ADD] Navbar

--- src/App.js
+++ src/App.js
@@ -20,9 +20,48 @@ const Button = styled.button`
 
 `;
 
+const Navbar = styled.div`
+  height: 60px;
+  border-bottom: 2px solid #333;
+  padding: 2em;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const LogoNav = styled.div`
+  background: blue;
+  padding: .5em 2em;
+  border: 2px solid #000;
+  width: 150px;
+`;
+
+const NavMenu = styled.div`
+  padding: .5em;
+  border-bottom: 2px solid #eee;
+`;
+
+const UL = styled.div`
+  display: flex;
+  list-style: none;
+`;
+
+const Ulli = styled.div`
+  margin-right: 1em;
+`;
+
 function App() {
   return (
      <div>
+       <Navbar>
+         <LogoNav>MySite</LogoNav>
+         <NavMenu>
+           <UL>
+             <Ulli><a href="#">Home</a></Ulli>
+             <Ulli><a href="#">About</a></Ulli>
+             <Ulli><a href="#">Contact</a></Ulli>
+           </UL>
+         </NavMenu>
+       </Navbar>
        <Button>I'm Button</Button>
      </div>
   );
