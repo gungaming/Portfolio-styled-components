e2ddc6a29c21bb0cf432493bf001b55cb1385f22
Sharinogan <voragun2543@gmail.com>
2021-06-10 18:51:24 +0700

    [ADD] comment button
    comment github desktop

--- src/App.js
+++ src/App.js
@@ -1,68 +1,30 @@
-import './App.css';
 import styled from 'styled-components';
 
+// coomit use github desktop
 const Button = styled.button`
 
   cursor: pointer;
   background: transparent;
   font-size: 16px;
   border-radius: 3px;
-  color: #000;
-  border: 2px solid #000
+  color: ${props => (props.primary ? 'black' : 'red')};
+  border: ${props => (props.primary ? '2px solid black' : '2px solid pink')};
   margin: 0 1em;
   padding: 0.25em 1em;
   transition: 0.5s all ease-out;
 
   &:hover {
-    background-color: #000;
+    background-color: ${props => (props.primary ? 'black' : 'red')};
     color: #fff;
   }
 
 `;
 
-const Navbar = styled.div`
-  height: 60px;
-  border-bottom: 2px solid #333;
-  padding: 2em;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const LogoNav = styled.div`
-  background: blue;
-  padding: .5em 2em;
-  border: 2px solid #000;
-  width: 150px;
-`;
-
-const NavMenu = styled.div`
-  padding: .5em;
-  border-bottom: 2px solid #eee;
-`;
-
-const UL = styled.div`
-  display: flex;
-  list-style: none;
-`;
-
-const Ulli = styled.div`
-  margin-right: 1em;
-`;
-
 function App() {
   return (
      <div>
-       <Navbar>
-         <LogoNav>MySite</LogoNav>
-         <NavMenu>
-           <UL>
-             <Ulli><a href="#">Home</a></Ulli>
-             <Ulli><a href="#">About</a></Ulli>
-             <Ulli><a href="#">Contact</a></Ulli>
-           </UL>
-         </NavMenu>
-       </Navbar>
-       <Button>I'm Button</Button>
+       <Button>I'm Button1</Button>
+       <Button primary>I'm Button</Button>
      </div>
   );
 }
